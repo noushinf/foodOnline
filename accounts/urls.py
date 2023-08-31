e97e4410672dc547884db69b8446073649f12051
Noushin forouzandehfar <noushinforouzande@gmail.com>
2023-08-31 17:51:50 +0330

    vendor and customer register

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     # path('', views.myAccount),
     path('registerUser/', views.registerUser, name='registerUser'),
-      path('registerVendor/', views.registerVendor, name='registerVendor'),
+    path('registerVendor/', views.registerVendor, name='registerVendor'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -104,7 +104,7 @@ def registerVendor(request):
             # Send verification email
             mail_subject = 'Please activate your account'
             email_template = 'accounts/emails/account_verification_email.html'
-           # send_verification_email(request, user, mail_subject, email_template)
+            send_verification_email(request, user, mail_subject, email_template)
 
             messages.success(request, 'Your account has been registered sucessfully! Please wait for the approval.')
             return redirect('registerVendor')
@@ -173,3 +173,85 @@ def myAccount(request):
     return redirect(redirectUrl)
 
 
+
+
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+
+            # send reset password email
+            mail_subject = 'Reset Your Password'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
+            messages.success(request, 'Password reset link has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgot_password')
+    return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request, uidb64, token):
+    # validate the user by decoding the token and user pk
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('myAccount')
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    #orders = Order.objects.filter(user=request.user, is_ordered=True)
+   # recent_orders = orders[:5]
+   
+    return render(request, 'accounts/custDashboard.html')
+
+def vendorDashboard(request):
+    vendor = Vendor.objects.get(user=request.user)
+   # orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
+   # recent_orders = orders[:10]
+
+    # current month's revenue
+    current_month = datetime.datetime.now().month
+    #current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
+    current_month_revenue = 0
+    
+
+    # total revenue
+    total_revenue = 0
+    
+    return render(request, 'accounts/vendorDashboard.html')
